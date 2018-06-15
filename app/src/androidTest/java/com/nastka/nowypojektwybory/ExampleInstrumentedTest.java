17a3270e626b1d06e66b43986e9cfe765f9d5aa6
Dominik Wawrzkiewicz <dominikwawrzkiewicz141@gmail.com>
2018-06-15 11:43:35 +0200

    try

--- app/src/androidTest/java/com/nastka/nowypojektwybory/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nastka/nowypojektwybory/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.jvrekk.apkawyborcza;
+package com.nastka.nowypojektwybory;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -17,10 +17,10 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.jvrekk.apkawyborcza", appContext.getPackageName());
+        assertEquals("com.nastka.nowypojektwybory", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/nastka/nowypojektwybory/MainActivity.java
+++ app/src/main/java/com/nastka/nowypojektwybory/MainActivity.java
@@ -1,15 +1,13 @@
-package com.example.jvrekk.apkawyborcza;
+package com.nastka.nowypojektwybory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("DD","Dd");
         setContentView(R.layout.activity_main);
     }
 }
